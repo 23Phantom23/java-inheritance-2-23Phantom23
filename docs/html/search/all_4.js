1e8eb0868b47749014cd1bdb120793c54af51326
23Phantom23 <91052918+23Phantom23@users.noreply.github.com>
2022-06-02 12:57:42 +0300

    javadoc

---
+++ docs/html/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['leopard_0',['Leopard',['../classdomain_1_1_leopard.html',1,'domain.Leopard'],['../classdomain_1_1_leopard.html#abd25f19a358009e63eb028f7301a7668',1,'domain.Leopard.Leopard(String name, int weight, String kind)'],['../classdomain_1_1_leopard.html#a04b8aafda07240fda9db2caf09210efe',1,'domain.Leopard.Leopard()'],['../classdomain_1_1_leopard.html#a1cb8811fb34e96397ddbc28610672365',1,'domain.Leopard.Leopard(String name)']]],
+  ['leopard_2ejava_1',['Leopard.java',['../_leopard_8java.html',1,'']]]
+];
